f47578be7250ec2cb970e056eb4f5bf8e73c223b
trsif <pepefava1@gmail.com>
2021-06-11 11:12:39 -0400

    intial commit

---
+++ src/test/java/com/qa/domain/SimpleCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
